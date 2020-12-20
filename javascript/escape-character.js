3b9ccc7df679e1171d19b2d3b93473b7dc9987e7
Bhavya SIngh <bhavya4official@gmail.com>
2020-12-20 21:30:43 +0530

    Create escape-character.js

---
+++ javascript/escape-character.js
@@ -0,0 +1,24 @@
+// Escaping Literal Quotes in StringsPassed
+
+// In JavaScript, you can escape a quote from considering it as an end of string quote by placing a backslash (\) in front of the quote.
+
+var sampleStr = "Alan said, \"Peter is learning JavaScript\".";
+
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+// String values in JavaScript may be written with single or double quotes, as long as you start and end with the same type of quote. Unlike some other programming languages, single and double quotes work the same in JavaScript.
+
+doubleQuoteStr = "This is a string"; 
+singleQuoteStr = 'This is also a string';
+
+// The reason why you might want to use one type of quote over the other is if you want to use both in a string. This might happen if you want to save a conversation in a string and have the conversation in quotes. Another use for it would be saving an <a> tag with various attributes in quotes, all within a string.
+
+conversation = 'Finn exclaims to Jake, "Algebraic!"';
+
+// However, this becomes a problem if you need to use the outermost quotes within it. Remember, a string has the same kind of quote at the beginning and end. But if you have that same quote somewhere in the middle, the string will stop early and throw an error.
+
+goodStr = 'Jake asks Finn, "Hey, let\'s go on an adventure?"'; 
+badStr = 'Finn responds, "Let's go!"'; // Throws an error
+
+// In the goodStr above, you can use both quotes safely by using the backslash \ as an escape character.
