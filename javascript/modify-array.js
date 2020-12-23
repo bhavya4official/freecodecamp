c8c3f1eb2f6f915f2235eaa57a8631cd2fd2cbda
Bhavya SIngh <bhavya4official@gmail.com>
2020-12-23 13:11:23 +0530

    Create modify-array.js

---
+++ javascript/modify-array.js
@@ -0,0 +1,6 @@
+/* Modify Array Data With Indexes */
+
+// Unlike strings, the entries of arrays are mutable and can be changed freely.
+
+var ourArray = [50,40,30];
+ourArray[0] = 15; // equals [15,40,30]
