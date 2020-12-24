ee05fa73523ea0ddc6f5915222f1a1d1b9735a41
Bhavya SIngh <bhavya4official@gmail.com>
2020-12-24 20:03:07 +0530

    Update return-value.js

--- javascript/return-value.js
+++ javascript/return-value.js
@@ -11,3 +11,18 @@ var answer = plusThree(5); // 8
 We can pass values into a function with arguments.
 We can use a return statement to send a value back out of a function.
 **/
+
+/* Understanding Undefined Value returned from a Function */
+
+var sum = 0;
+function addSum(num) {
+  sum = sum + num;
+}
+addSum(3); // sum will be modified but returned value is undefined
+
+//addSum is a function without a return statement. The function will change the global sum variable but the returned value of the function is undefined.
+
+/**
+A function can include the return statement but it does not have to.
+In the case that the function doesn't have a return statement, when you call it, the function processes the inner code but the returned value is undefined.
+**/
