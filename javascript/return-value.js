c8696b0e36974e0ef9f118fbe4df6f567ace4d21
Bhavya SIngh <bhavya4official@gmail.com>
2020-12-24 19:48:20 +0530

    Create return-value.js

---
+++ javascript/return-value.js
@@ -0,0 +1,13 @@
+/* Return a Value from a Function with Return */
+
+function plusThree(num) {
+  return num + 3;
+}
+var answer = plusThree(5); // 8
+
+// plusThree takes an argument for num and returns a value equal to num + 3.
+
+/**
+We can pass values into a function with arguments.
+We can use a return statement to send a value back out of a function.
+**/
