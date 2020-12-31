20943ee42dda30d079a19fc33922f0007ec3f173
Bhavya SIngh <bhavya4official@gmail.com>
2020-12-31 16:23:56 +0530

    Update object.js

--- javascript/object.js
+++ javascript/object.js
@@ -25,3 +25,8 @@ var anotherObject = {
 };
 
 // However, if your object has any non-string properties, JavaScript will automatically typecast them as strings.
+
+/**
+There are two ways to access the properties of an object: dot notation (.) and bracket notation ([]), similar to an array.
+Dot notation is what you use when you know the name of the property you're trying to access ahead of time.
+**/
